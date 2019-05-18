c2fd2f56771c3333946f2a7ac72d060e2ab61e56
VitaliyFeytser <vitaliyfeytser@gmail.com>
2019-05-18 13:33:59 -0700

    booksection double declaration removed

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import BookSection from "./components/BookSection";
 import FavSection from "./components/FavSection";
 import Nav from "./components/Nav";
-import BookSection from "./components/BookSection";
 import SignInSection from "./components/SignInSection";
 
 function App() {
